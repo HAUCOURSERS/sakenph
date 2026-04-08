08d6a6f98ec34704d791b117c41bc14ad3b0e310
j-lim45 <crimson4511@gmail.com>
2026-04-08 21:59:20 +0800

    Commented out supabase

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,15 @@ import 'package:flutter/material.dart';
 // import 'package:sakenph/auth_gate.dart';
 // import 'package:sakenph/login_register.dart';
 import 'package:sakenph/home_page.dart';
-
-import 'package:supabase_flutter/supabase_flutter.dart';
+// import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  // WidgetsFlutterBinding.ensureInitialized();
 
-  await Supabase.initialize(
-    url: 'https://tfycptprhagrgpxccjiy.supabase.co',
-    anonKey: 'sb_publishable_M6rcjMdAMWceiBpMkQQOVg_ejVFeKK7'
-  );
+  // await Supabase.initialize(
+  //   url: 'https://tfycptprhagrgpxccjiy.supabase.co',
+  //   anonKey: 'sb_publishable_M6rcjMdAMWceiBpMkQQOVg_ejVFeKK7'
+  // );
 
   runApp(const MyApp());
 }
