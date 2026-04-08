69149cef89d0600807417de5da9caf7c57da17a0
j-lim45 <crimson4511@gmail.com>
2026-04-08 23:26:20 +0800

    Updated ndk version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 android {
     namespace = "com.example.sakenph"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "28.1.13356709"
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
