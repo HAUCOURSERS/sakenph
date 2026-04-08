501df4a893e3d12bf1b23f15eb53ef0f7ab12f84
j-lim45 <crimson4511@gmail.com>
2026-04-08 21:59:37 +0800

    Removed ORS API

--- lib/ors_api.dart
+++
@@ -1,7 +0,0 @@
-class OpenRouteService {
-  String apiKey = 'apiKey';
-  Uri getRoute(String pointA, String pointB) {
-  
-    return Uri.parse('https://api.openrouteservice.org/v2/directions/driving-car?api_key=$apiKey&start=$pointA&end=$pointB');
-  }
-}
