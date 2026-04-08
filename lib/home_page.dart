e8ca047c947223e5b0f1ed1f9f1138970ae6bbc3
j-lim45 <crimson4511@gmail.com>
2026-04-08 23:26:39 +0800

    test integration with backend

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,6 +3,7 @@ import 'package:sakenph/map_widget.dart';
 import 'package:sakenph/settings_page.dart';
 import 'package:http/http.dart' as http;
 import 'dart:developer';
+import 'dart:convert';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,6 +13,25 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePage extends State<HomePage> {
+  late Future<Map<dynamic, dynamic>> json;
+
+  @override
+  void initState() {
+    super.initState();
+    json = fetchData();
+
+  }
+
+  Future<Map<String, dynamic>> fetchData() async {
+    String localIp = "192.168.100.7";
+    final response = await http.get(Uri.parse('http://${localIp}:8000/flutterTest'));
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Failed to load JSON');
+    }
+  }
     
   @override
   Widget build(BuildContext context) {
@@ -66,6 +86,28 @@ class _HomePage extends State<HomePage> {
                   ),
                   child: Center(
                     child: Text("To:"),
+                  )
+                ),
+
+                Container(
+                  width: 320, height: 40,
+                  margin: EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey, width:1),
+                    color: const Color.fromARGB(255, 227, 241, 255),
+                    borderRadius: BorderRadius.circular(32)
+                  ),
+                  child: Center(
+                    child: FutureBuilder(
+                      future: json, 
+                      builder: (context, snapshot) {
+                        if (snapshot.hasData) {
+                          return Text(snapshot.data!['key']);
+                        } else {
+                          return Text('${snapshot.error}');
+                        }
+                      }
+                      ),
                   )
                 ),
               ],
