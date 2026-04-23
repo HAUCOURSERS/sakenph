3e85cc454b7349045a14e49a68c98f3abf514071
Josef Miko Urquico <josef.urquico@gmail.com>
2026-04-23 13:59:02 +0800

    Added input for "from location", refactors and improved location selector

--- lib/providers/provider_selected_loc.dart
+++ lib/providers/provider_selected_loc.dart
@@ -3,8 +3,16 @@ import 'package:maplibre_gl/maplibre_gl.dart';
 
 /// Storing target lat long
 class LatLongProvider with ChangeNotifier {
+  LatLng? _fromLoc;
+  LatLng? get fromLoc => _fromLoc;
+
   LatLng? _toLoc;
   LatLng? get toLoc => _toLoc;
+
+  void setFromLoc(double lat, double lon) {
+    _fromLoc = LatLng(lat, lon);
+    notifyListeners();
+  }
 
   void setToLoc(double lat, double lon) {
     _toLoc = LatLng(lat, lon);
