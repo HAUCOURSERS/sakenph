06537a6d40c3c272982572796e726d6eae0b0b26
Josef Miko Urquico <josef.urquico@gmail.com>
2026-05-31 15:09:33 +0800

    progress for now

--- lib/features/home_page/functions.dart
+++ lib/features/home_page/functions.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:sakenph/classes/nominatim_response.dart';
 import 'package:http/http.dart' as http;
 
@@ -72,4 +74,44 @@ Widget loadingDisplay() {
     },
     child: Container(height: 30, child: const Text("Loading..")),
   );
+}
+
+/// To get location perms
+Future<bool> handleLocationPermission(BuildContext context) async {
+  if (!context.mounted) return false;
+  bool serviceEnabled;
+  LocationPermission permission;
+
+  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text(
+          'Location services are disabled. Please enable the services',
+        ),
+      ),
+    );
+    return false;
+  }
+  permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Location permissions are denied')),
+      );
+      return false;
+    }
+  }
+  if (permission == LocationPermission.deniedForever) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text(
+          'Location permissions are permanently denied, we cannot request permissions.',
+        ),
+      ),
+    );
+    return false;
+  }
+  return true;
 }
