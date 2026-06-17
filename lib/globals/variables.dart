b0c4bf1836f343bc1fbb69ede79876ebf7558497
Josef Miko Urquico <josef.urquico@gmail.com>
2026-06-17 18:30:37 +0800

    a

--- lib/globals/variables.dart
+++ lib/globals/variables.dart
@@ -1,4 +1,4 @@
 /// Stores information that needs to be shared across the project
 /// ex: import 'package:sakenph/globals/global_vars.dart' as global_vars show localIP;
-String _localIP = "10.242.242.187";
+String _localIP = "192.168.68.61";
 String get localIP => _localIP;
