3473dd1763540a3b1ba0abce47529813caf78e0a
Josef Miko Urquico <josef.urquico@gmail.com>
2026-08-08 18:12:34 +0800

    removed redundant code for widgets.dart
    following removed widgets are already moved to content_state_switcher.dart

--- lib/features/foreground_widget/widgets.dart
+++ lib/features/foreground_widget/widgets.dart
@@ -1,14 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:maplibre_gl/maplibre_gl.dart';
 import 'package:sakenph/features/foreground_widget/functions.dart';
-import 'package:sakenph/features/foreground_widget/widgets/active_route_terminator.dart';
-import 'package:sakenph/features/foreground_widget/widgets/from_location_search_bar.dart';
-import 'package:sakenph/features/foreground_widget/widgets/preview_window_for_suggested_path.dart';
-import 'package:sakenph/features/foreground_widget/widgets/route_opener_button.dart';
-import 'package:sakenph/features/foreground_widget/widgets/selected_location_decision_helper.dart';
-import 'package:sakenph/features/foreground_widget/widgets/to_location_search_bar.dart';
-import 'package:sakenph/globals/enums.dart';
-import 'package:sakenph/providers/provider_system_vars.dart';
 import 'package:provider/provider.dart';
 
 import '../../providers/provider_map_helper.dart';
@@ -17,95 +9,6 @@ import '../../providers/provider_map_helper.dart';
 import 'package:sakenph/classes/jeepney_route.dart';
 import 'package:sakenph/classes/terminal_class.dart';
 import 'package:sakenph/api/backend_service.dart';
-
-/// The main widget for the Foreground. Any widgets that are needed to be displayed
-/// at the top of the main widget's stack is written here.
-class ForegroundWidget extends StatefulWidget {
-  const ForegroundWidget({super.key});
-
-  @override
-  State<ForegroundWidget> createState() => _ForegroundWidgetState();
-}
-
-// class _ForegroundWidgetState extends State<ForegroundWidget> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return SafeArea(
-//       child: Stack(children: [_ForegroundWidgetContentRenderer()]),
-//     );
-//   }
-// }
-
-// Added to import the backend service to use its functions for querying shortest paths and fetching jeepney routes.
-class _ForegroundWidgetState extends State<ForegroundWidget> {
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Stack(
-        children: [
-          _ForegroundWidgetContentRenderer(),
-          JeepneyRouteFloatingControl(),
-        ],
-      ),
-    );
-  }
-}
-
-/// Relies on the value of [SystemVariablesProvider.appCurrentState] to
-/// decide what to render.
-class _ForegroundWidgetContentRenderer extends StatelessWidget {
-  const _ForegroundWidgetContentRenderer();
-
-  @override
-  Widget build(BuildContext context) {
-    MapHelperProvider mapHelperProvider = context.read<MapHelperProvider>();
-    bool isFromLocDetailsEmpty = context.select<MapHelperProvider, bool>(
-      (value) => value.getIsFromLocationDetailsEmpty,
-    );
-    bool hasSearchedForRoutes = context.select<MapHelperProvider, bool>(
-      (value) => !value.getIsSuggestedShortestPathsEmpty,
-    );
-    SystemState systemState = context
-        .select<SystemVariablesProvider, SystemState>(
-          (value) => value.appCurrentState,
-        );
-    bool backgroundWidgetVisibility = context
-        .select<SystemVariablesProvider, bool>(
-          (value) => value.backgroundWidgetVisibility,
-        );
-    switch (systemState) {
-      case SystemState.gatheringFromLoc:
-      case SystemState.gatheringToLoc:
-      case SystemState.waitingForBackendResponse:
-      case SystemState.showSuggestedRoutes:
-      case SystemState.backendRequestFail:
-        return (isFromLocDetailsEmpty)
-            ? FromLocationSearchBar()
-            : Column(
-                children: [
-                  FromLocationSearchBar(),
-                  ToLocationSearchBar(),
-                  AnimatedSwitcher(
-                    duration: const Duration(milliseconds: 300),
-                    child: (hasSearchedForRoutes && !backgroundWidgetVisibility)
-                        ? RouteOpenerButton()
-                        : SizedBox.shrink(),
-                  ),
-                ],
-              );
-      case SystemState.peekAtRoute:
-        return PreviewWindowForSuggestedPath(
-          routeDetails: mapHelperProvider.getSuggestedShortestPaths,
-        );
-      case SystemState.hideWidgets:
-        return SizedBox.shrink();
-      case SystemState.isCurrentlyTravelling:
-        return ActiveRouteTerminator();
-      case SystemState.confirmingLocationSelection:
-        return SelectedLocationDecisionHelper();
-    }
-  }
-}
 
 /// A floating button that can be dragged around the screen.
 /// When pressed, it opens a panel that shows the list of jeepney routes and their visibility status on the map.
