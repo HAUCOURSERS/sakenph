4bdf5f25e782d8c8565e24e051060d14b213bcd4
Josef Miko Urquico <josef.urquico@gmail.com>
2026-08-14 16:29:48 +0800

    fixed import issue

--- lib/features/background_widget/widgets/use_current_location_button.dart
+++ lib/features/background_widget/widgets/use_current_location_button.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:sakenph/globals/enums.dart';
-import 'package:sakenph/features/foreground_widget/functions.dart'
-    show handleLocationPermission;
+import 'package:sakenph/globals/functions/system/permissions.dart';
 import 'package:sakenph/globals/functions/utils_responsiveness.dart';
 import 'package:sakenph/providers/provider_map_helper.dart';
 import 'package:sakenph/providers/provider_search_details.dart';
@@ -29,79 +28,79 @@ class _UseCurrentLocationButtonState extends State<UseCurrentLocationButton> {
         children: [
           SizedBox(height: responsiveSizeHeight(16)),
           AnimatedContainer(
-        duration: Duration(milliseconds: 200),
-        color: _showColor ? Colors.grey.shade300 : Colors.transparent,
-        child: GestureDetector(
-          onTap: () async {
-            bool locationSet = await handleLocationPermission(context);
-            if (!locationSet) return;
-            setState(() {
-              _showColor = true;
-            });
-            SearchDetailsProvider searchDetailsProvider = context
-                .read<SearchDetailsProvider>();
-            MapHelperProvider mapHelperProvider = context
-                .read<MapHelperProvider>();
-            SystemVariablesProvider systemVariablesProvider = context
-                .read<SystemVariablesProvider>();
-            mapHelperProvider.useCurrentUserGeoLocAsOrigin();
-            systemVariablesProvider.setAppCurrentState =
-                SystemState.gatheringToLoc;
-            searchDetailsProvider.requestFocusTowardsLocTextfield();
+            duration: Duration(milliseconds: 200),
+            color: _showColor ? Colors.grey.shade300 : Colors.transparent,
+            child: GestureDetector(
+              onTap: () async {
+                bool locationSet = await handleLocationPermission(context);
+                if (!locationSet) return;
+                setState(() {
+                  _showColor = true;
+                });
+                SearchDetailsProvider searchDetailsProvider = context
+                    .read<SearchDetailsProvider>();
+                MapHelperProvider mapHelperProvider = context
+                    .read<MapHelperProvider>();
+                SystemVariablesProvider systemVariablesProvider = context
+                    .read<SystemVariablesProvider>();
+                mapHelperProvider.useCurrentUserGeoLocAsOrigin();
+                systemVariablesProvider.setAppCurrentState =
+                    SystemState.gatheringToLoc;
+                searchDetailsProvider.requestFocusTowardsLocTextfield();
 
-            searchDetailsProvider.setFromLocTextfieldText =
-                "Your Current Location";
-            await Future.delayed(Duration(milliseconds: 100));
-            setState(() {
-              _showColor = false;
-            });
-          },
-          child: Container(
-            width: responsiveSizeWidth(
-              MediaQuery.sizeOf(context).width * 0.8,
-              500,
-            ),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              border: Border.all(
-                color: Colors.black,
-                width: responsiveSizeWidth(1),
-              ),
-              borderRadius: BorderRadius.circular(12),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withValues(alpha: 0.06),
-                  blurRadius: 8,
-                  offset: Offset(0, 2),
+                searchDetailsProvider.setFromLocTextfieldText =
+                    "Your Current Location";
+                await Future.delayed(Duration(milliseconds: 100));
+                setState(() {
+                  _showColor = false;
+                });
+              },
+              child: Container(
+                width: responsiveSizeWidth(
+                  MediaQuery.sizeOf(context).width * 0.8,
+                  500,
                 ),
-              ],
-            ),
-            padding: EdgeInsets.symmetric(
-              horizontal: responsiveSizeHeight(16),
-              vertical: responsiveSizeHeight(14),
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  Icons.location_on,
-                  size: responsiveSizeHeight(20),
-                  color: Colors.black87,
-                ),
-                SizedBox(width: 8),
-                Text(
-                  "Press to use your location",
-                  style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                    fontSize: responsiveSizeHeight(17),
-                    color: Colors.black87,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(
+                    color: Colors.black,
+                    width: responsiveSizeWidth(1),
                   ),
+                  borderRadius: BorderRadius.circular(12),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withValues(alpha: 0.06),
+                      blurRadius: 8,
+                      offset: Offset(0, 2),
+                    ),
+                  ],
                 ),
-              ],
+                padding: EdgeInsets.symmetric(
+                  horizontal: responsiveSizeHeight(16),
+                  vertical: responsiveSizeHeight(14),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.location_on,
+                      size: responsiveSizeHeight(20),
+                      color: Colors.black87,
+                    ),
+                    SizedBox(width: 8),
+                    Text(
+                      "Press to use your location",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: responsiveSizeHeight(17),
+                        color: Colors.black87,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
           ),
-        ),
-      ),
         ],
       ),
     );
